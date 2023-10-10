aac054d0bbd5e1d12acd159c19248253df595e69
tamariam <>
2023-10-10 14:16:10 +0000

    start my project with welcome message to user,asking user if wants to make appointment or not

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
- print('welcome to hair-salon, its really swould you like to book an appointment?(yes/no')
- answer=input('').lower()
+
+
+print('Welcome to the hair-beauty booking app')
+answer = input('Would you like to book an appointment? (yes/no): ')
+
+if answer.lower() != 'yes':
+    ('See you next time then')
+    quit()
+
+print('Great!')
