7ae7ea50d6a43935b769e27d866c7a608be88fbb
tamariam <>
2023-10-10 14:04:31 +0000

    innitial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+ print('welcome to hair-salon, its really swould you like to book an appointment?(yes/no')
+ answer=input('').lower()
