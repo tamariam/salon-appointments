4794f616fb3b70eab7fc563710e21c3c4dd37c57
tamariam <>
2023-10-11 16:03:49 +0000

    implement logic to find out if user enteres valid time or not, also , when user choose date after that is asked to enter name and time

--- run.py
+++ run.py
@@ -3,16 +3,16 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from datetime import datetime
 
+
 print('Welcome to the hair-beauty booking app')
 user_choice = int(input('please select one option below \n 1) book an appointment \n 2) view availibilitys for today\n 3)cacell appointment\n 4)search appointment\n '))
-if user_choice == 1:
-    while True:    
-        date = input('Please let us know when you want an appointment (dd/mm/yyyy): ').lower()
+if user_choice == 1 :
+    while True: 
+         date = input('Please let us know when you want an appointment (dd/mm/yyyy): ').lower()   
+       # Split the input date string
+         date_obj = date.split('/')
 
-        # Split the input date string
-        date_obj = date.split('/')
-
-        if len(date_obj) == 3:
+         if len(date_obj) == 3:
             day, month, year = map(int, date_obj)
             current_year = datetime.now().year
             today = datetime.now().date()
@@ -29,16 +29,24 @@ if user_choice == 1:
                     print(appointment_date)
                     if appointment_date >= today:
                         if appointment_date.weekday() < 5:
-                            print('Good job')
-                            break
+                            name=input('please enter your name ').strip()
+                            time=input('plese enter time ').strip()
+                            hours,minutes=map(int,time.split (':'))
+                                
+                            if 9<=hours<18 and 0 <=minutes <60:
+                                print('your appointment has been booked ')
+                                break
+                            else:
+                                print('salon is closed before 9am and after 18pm')
+    
                         else:
                             print('Please choose another option because we are closed on weekends')
                     else:
                         print('date is in past please enter  future or todays date ')
                 else:
-                    print('Invalid date format. Please use valid dd/mm/yy format.')       
+                    print('Invalid date format. Please use valid dd/mm/yyyy format.')       
             else:
-                print('Invalid date format. Please use valid dd/mm/yy format.')
+                print('Invalid date format. Please use valid dd/mm/yyyy format.')
 
 
 
